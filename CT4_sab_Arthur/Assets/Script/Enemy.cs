1ba91aca68413aedd1d0fd7c48316d2fd81c3e81
mateusctrlplay <m.lima@ctrlplay.com.br>
2026-04-18 12:31:03 -0300

    projeto do arthur

---
+++ CT4_sab_Arthur/Assets/Script/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform personagem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(personagem);
+        transform.Rotate(Vector3.right, 90);
+    }
+}
---
+++ CT4_sab_Arthur/Assets/Script/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
+// i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear
