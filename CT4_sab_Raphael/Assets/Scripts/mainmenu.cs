b28cbb599a5a75c6ea424895477a6e742eab4612
mateusctrlplay <m.lima@ctrlplay.com.br>
2026-04-18 14:28:59 -0300

    projeto de raphael

---
+++ CT4_sab_Raphael/Assets/Scripts/mainmenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+
+    public Void StartGame(){
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
