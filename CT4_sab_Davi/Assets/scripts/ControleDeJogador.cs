df972308e42ce75a2faca0049400747f1ecc4ae2
mateusctrlplay <m.lima@ctrlplay.com.br>
2026-04-18 14:20:45 -0300

    projeto de davi

---
+++ CT4_sab_Davi/Assets/scripts/ControleDeJogador.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDeJogador : MonoBehaviour
+{
+    public float sensibilidadeDeMouse = 100f;
+    public Transform jogador; 
+    float rotacaoX = 0f;
+
+    public CharacterController controller;
+    public float velocidade = 4.04f;
+    public float gravidade = -9.81f;
+
+    Vector3 aceleracao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidadeDeMouse * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidadeDeMouse * Time.deltaTime;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move (move * velocidade * Time.deltaTime);
+
+        aceleracao.y += gravidade * Time.deltaTime;
+        controller.Move(aceleracao * Time.deltaTime);
+
+        rotacaoX -= mouseY;
+        rotacaoX = Mathf.Clamp(rotacaoX, -90f, 90);
+        
+        transform.localRotation = Quaternion.Euler(rotacaoX, 0f, 0f);
+        jogador.Rotate(Vector3.up * mouseX);
+   
+   
+    }
+}
