51b55635a8c6c26ae25d69c4f9e0b28c5dbbbb6e
mateusctrlplay <m.lima@ctrlplay.com.br>
2026-04-25 12:29:14 -0300

    aula 25.04.2026 funcoes dos botoes menu

--- CT4_sab_Arthur/Assets/Script/MainMenu.cs
+++ CT4_sab_Arthur/Assets/Script/MainMenu.cs
@@ -9,6 +9,16 @@ public class Menu : MonoBehaviour
     public void StartGame(){
         SceneManager.LoadScene("SampleScene");
     }
+        public void Credits(){
+        SceneManager.LoadScene("Credits");
+    }
+        public void Quit(){
+        Application.Quit();
+        
+    }
+        public void BackToMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
 }
 
 // i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear i am the gummy bear, i am the gummy bear, i am the gummy bear, i am the gummy bear
